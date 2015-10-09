16e1dc48ff89d922eb4e0351cead86a562d76723
Ken Hibino <ken.hibino7@gmail.com>
2015-10-09 18:20:38 -0400

    Add full_title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | Textbook App"
+    assert_select "title", "Textbook App"
   end
 
   test "should get help" do
