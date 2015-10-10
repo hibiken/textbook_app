8101b4396ee20f719573f6bf492b388fcbb3773d
Ken Hibino <ken.hibino7@gmail.com>
2015-10-10 13:05:00 -0400

    Add secure password to User model

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'https://rubygems.org'
 
 
 gem 'rails',          '4.2.4'
+gem 'bcrypt',         '3.1.7'
 gem 'sass'
 gem 'sass-rails',     '5.0.1'
 gem 'bootstrap-sass', '3.2.0.0'
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,7 +4,8 @@ class UserTest < ActiveSupport::TestCase
   
   # Create a valid user object.
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -30,6 +31,11 @@ class UserTest < ActiveSupport::TestCase
 
   test "email should not be too long" do
     @user.email = 'a' * 244 + '@example.com'
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = 'a' * 5
     assert_not @user.valid?
   end
 
