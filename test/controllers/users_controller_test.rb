83cc33478312c353797bbb25d2e57c10bfaea174
Ken Hibino <ken.hibino7@gmail.com>
2015-10-11 12:20:55 -0400

    Add ability to delete users for admin user

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -43,4 +43,51 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to root_url
   end
 
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch :update, id: @other_user, user: { password:              'password',
+                                            password_confirmation: 'password',
+                                            admin: true }
+    assert_not @other_user.reload.admin?
+  end
+
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as @other_user
+    assert_no_difference 'User.count' do
+      delete :destroy, id: @user
+    end
+    assert_redirected_to root_url
+  end
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
