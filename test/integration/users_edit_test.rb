c91f103aebd78e15536afa3c0f559940a2de13bc
Ken Hibino <ken.hibino7@gmail.com>
2015-10-11 09:54:15 -0400

    Add before filters to edit, update actions in Users controller to confirms the correct user to be logged in

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -7,6 +7,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "unsuccessful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), user: { name: '',
@@ -17,8 +18,9 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   end
 
   test "successful edit" do
+    log_in_as(@user)
     get edit_user_path(@user)
-    assert_template 'users/path'
+    assert_template 'users/edit'
     name = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), user: { name: name, 
