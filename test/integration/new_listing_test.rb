bf5cfa9464ff637e7de49f248b2961c0fe186d71
Ken Hibino <ken.hibino7@gmail.com>
2015-10-13 11:52:31 -0400

    Update new listing test

--- test/integration/new_listing_test.rb
+++ test/integration/new_listing_test.rb
@@ -3,24 +3,29 @@ require 'test_helper'
 class NewListingTest < ActionDispatch::IntegrationTest
   
   def setup
-    @user = users(:ken)
+    @user    = users(:ken)
+    @subject = subjects(:computer_science) 
     log_in_as @user
   end
 
   test "successful new listing" do
     get new_textbook_path
     assert_template 'textbooks/new'
-    post textbooks_path, textbook: { title: "Intro to CS", price: 50.00, subject_id: 1, description: "This is great" }
+    post textbooks_path, textbook: { title: "Intro to CS", price: 50.00, subject_id: @subject.id, description: "This is great" }
     assert_redirected_to @user
     follow_redirect!
     assert_match /Intro to CS/i, response.body
     assert_match /\$50.00/, response.body
+    # New listing is showing in correct subject show page
+    get subject_path(@subject)
+    assert_template 'subjects/show'
+    assert_match /Intro to CS/i, response.body
   end
 
   test "unsuccessful listing" do
     get new_textbook_path
     assert_template 'textbooks/new'
-    post textbooks_path, textbook: { title: '', price: -30 }
+    post textbooks_path, textbook: { title: '', price: -30, subject_id: @subject.id }
     assert_template 'textbooks/new'
     assert_select 'div#error_explanation'
     get user_path(@user.id)
