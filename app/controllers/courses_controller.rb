3f6f9823761676e26585392f1763193abc58151f
Ken Hibino <ken.hibino7@gmail.com>
2015-10-14 21:54:46 -0400

    Update wishlist page to list all the sellers

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,7 +2,7 @@ class CoursesController < ApplicationController
   before_action :logged_in_user
 
   def index
-    @courses = current_user.courses
+    @courses = current_user.courses.includes(:textbooks)
   end
 
   def show
