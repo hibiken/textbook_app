906af687c40d9f8720eb331c6abeeb59dc035b80
Ken Hibino <ken.hibino7@gmail.com>
2015-10-21 18:27:37 -0400

    Add after_action hook to comments#create for notification & add bell icon to header

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,12 @@
 class CommentsController < ApplicationController
   before_action :logged_in_user
+  after_action  :push_notification, only: :create
 
   def create
-    textbook = Textbook.find(params[:comment][:textbook_id])
+    @textbook = Textbook.find(params[:comment][:textbook_id])
     @comment = current_user.comments.build(comment_params)
     if @comment.save
-      redirect_to textbook 
+      redirect_to @textbook 
     else
       flash[:danger] = "Something went wrong when posting your comment. Please try again."
       render 'textbooks/show'
@@ -18,5 +19,11 @@ class CommentsController < ApplicationController
     def comment_params
       params.require(:comment).permit(:message, :textbook_id)
     end
+
+    def  push_notification
+      message = "#{current_user.name} commented on your listing."
+      Notification.create(user_id: @textbook.user.id, message: message)
+    end
+
 
 end
