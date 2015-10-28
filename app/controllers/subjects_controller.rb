fefaf0818e378c7c1c2ec1ea2efdc5f4a51dfa4d
Ken Hibino <ken.hibino7@gmail.com>
2015-10-28 17:44:24 -0400

    Add pagination to subjects#show page

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -1,6 +1,6 @@
 class SubjectsController < ApplicationController
   def show
     @subject   = Subject.friendly.find(params[:id])
-    @textbooks = @subject.textbooks
+    @textbooks = @subject.textbooks.paginate(page: params[:page])
   end
 end
